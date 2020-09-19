8ef8b3d4788e87dd2803eb3694bb10c1d7d8d1b5
Corey Rowe <coreyr1196@gmail.com>
2020-09-19 17:26:27 -0400

    Read and write test data

--- app/src/main/java/com/grizzhacks/dartreader/MainActivity.java
+++ app/src/main/java/com/grizzhacks/dartreader/MainActivity.java
@@ -1,16 +1,20 @@
 package com.grizzhacks.dartreader;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.nfc.NfcAdapter;
 import android.nfc.Tag;
+import android.nfc.tech.MifareUltralight;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+import java.nio.charset.StandardCharsets;
 
+public class MainActivity extends AppCompatActivity {
 
     // Credit to this StackOverflow post for the NFC boilerplate code
     // https://stackoverflow.com/questions/5546932/how-to-read-and-write-android-nfc-tags
@@ -51,16 +55,65 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onNewIntent(Intent intent) {
         super.onNewIntent(intent);
-        getTagInfo(intent);
-    }
 
-    private void getTagInfo(Intent intent) {
         Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
 
         if (tag != null) {
-            Log.d("NFC TAG:", tag.toString());
-        }else{
-            Log.wtf("NFC TAG:", "Couldn't read.");
+            final MifareUltralight ultralight = MifareUltralight.get(tag);
+
+            if (ultralight != null) {
+
+                // You can only run one of these. You can't open two connections simultaneously.
+
+                // WARNING: The writeTag() method will overwrite activation information.
+                // Do not use on a DART pass you wish to keep.
+
+                // writeTagInfo(ultralight);
+
+                getTagInfo(ultralight);
+            }
         }
+    }
+
+    private void writeTagInfo(final MifareUltralight ultralight) {
+        Thread thread = new Thread(
+                new Runnable() {
+                    @RequiresApi(api = Build.VERSION_CODES.O)
+                    @Override
+                    public void run() {
+                        try {
+                            ultralight.connect();
+
+                            // Test writing data to a userdata page on the NFC tag
+                            ultralight.writePage(3, "test".getBytes(StandardCharsets.UTF_8));
+
+                            ultralight.close();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+        );
+        thread.start();
+    }
+
+    private void getTagInfo(final MifareUltralight ultralight) {
+        Thread thread = new Thread(
+                new Runnable() {
+                    @RequiresApi(api = Build.VERSION_CODES.O)
+                    @Override
+                    public void run() {
+                        try {
+                            ultralight.connect();
+                            byte[] byteArray = ultralight.readPages(3);
+                            Log.d("DATA: ", new String(byteArray, StandardCharsets.UTF_8));
+                            ultralight.close();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+        );
+        thread.start();
     }
 }
